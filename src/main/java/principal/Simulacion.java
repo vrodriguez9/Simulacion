b304951f36748787217e03366550a2b342904ace
vrodriguez9 <83665852+vrodriguez9@users.noreply.github.com>
2022-11-20 14:15:45 -0600

    Caso de prueba Valeria (Farmacias)

--- src/main/java/principal/Simulacion.java
+++ src/main/java/principal/Simulacion.java
@@ -1,38 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package principal;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-/**
- *
- * @author nitzyb
- */
 public class Simulacion {
 
     public static void main(String[] args) {
         casoderoger();
+        //casoDeValeria();
     }
-    static void casoderoger()
-    {
+
+    static void casoderoger() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.fischelenlinea.com/index");
         driver.manage().window().maximize();
-        
+
         driver.findElement(By.id("offersBtn")).click();
         driver.findElement(By.cssSelector("body > div.main > div.ng-scope > div.row.position-relative > section > div.filter > form > div > div:nth-child(5) > div > label")).click();
-         
+
         driver.findElement(By.cssSelector("#Combosubcategories")).click();
         driver.findElement(By.xpath("//*[@id=\"Combosubcategories\"]/option[2]")).click();
     }
-   
+
+    static void casoDeValeria() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.fischelenlinea.com/index");
+        driver.manage().window().maximize();
+
+        WebElement farmacias = driver.findElement(By.cssSelector("a.nav-link:nth-child(5)"));
+        farmacias.click();
+
+        driver.findElement(By.id("PronvinceCombo")).click();
+        driver.findElement(By.id("3")).click();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.id("CantonCombo")).click();
+        driver.findElement(By.cssSelector("#\\34 7")).click();
+    }
+
 }
