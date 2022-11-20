fa7b9a8abb10e93bd0320808b5006b68a2a57188
rogervasquezb <rvasquez0129@gmail.com>
2022-11-20 11:00:50 -0600

    Actualizacion: Caso de prueba Roger (Ofertas)

--- src/main/java/principal/Simulacion.java
+++ src/main/java/principal/Simulacion.java
@@ -5,6 +5,13 @@
 
 package principal;
 
+import java.time.Duration;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 /**
  *
  * @author nitzyb
@@ -12,6 +19,20 @@ package principal;
 public class Simulacion {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        casoderoger();
     }
+    static void casoderoger()
+    {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.fischelenlinea.com/index");
+        driver.manage().window().maximize();
+        
+        driver.findElement(By.id("offersBtn")).click();
+        driver.findElement(By.cssSelector("body > div.main > div.ng-scope > div.row.position-relative > section > div.filter > form > div > div:nth-child(5) > div > label")).click();
+         
+        driver.findElement(By.cssSelector("#Combosubcategories")).click();
+        driver.findElement(By.xpath("//*[@id=\"Combosubcategories\"]/option[2]")).click();
+    }
+   
 }
